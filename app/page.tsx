155627197809098877d72792d3d9701928b214d9
manonja <manon.jacquin@gmail.com>
2023-12-14 18:24:08 +0100

    feat: improve main styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import { Navbar } from "./components/navbar";
-import { Fragment } from "react";
-
 export default function Home() {
-  return (
-    <Fragment>
-      <Navbar />
-      <main></main>
-      <header></header>
-    </Fragment>
-  );
+  return <main></main>;
 }
