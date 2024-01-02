d19039513c58c1c070c46c62f369d634ec447fc9
manonja <manon.jacquin@gmail.com>
2024-01-02 16:47:06 +0100

    feat: cleanup unecessary props

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/navbar";
 import { Footer } from "./components/footer";
-import { getProjects } from "../lib/prisma";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export async function getServerSideProps() {
-  return {
-    props: {
-      projects: getProjects,
-    }
-  };
-}
 
 export const metadata: Metadata = {
   title: "Anton Bossenbroek",
