90321de31815df5b6b67916b3a50f6bb5b2492bc
manonja <manon.jacquin@gmail.com>
2024-01-02 10:29:55 +0100

    feat: handle error at root level

---
+++ app/error.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react'
+
+interface Props {
+    error: Error;
+    reset: () => void;
+}
+
+export const ErrorPage = ({error, reset}: Props) => {
+  return (
+    <>
+        <div>An unexpected error has occurred</div>
+        <button className="btn" onClick={() => reset()}>Retry</button>
+    </>
+  )
+}
+ export default ErrorPage
