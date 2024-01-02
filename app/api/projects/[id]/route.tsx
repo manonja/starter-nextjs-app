d3b0fc5b9e81d13a7e6d814a63cbc893406b7e16
manonja <manon.jacquin@gmail.com>
2024-01-02 14:15:58 +0100

    feat: WIP building API - hardcoded

--- app/api/projects/[id]/route.tsx
+++ app/api/projects/[id]/route.tsx
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
 // Adding NextRequest prevents caching
 export function GET(request: NextRequest, {params}: {params: {id: string}}) {
 
@@ -8,8 +7,6 @@ export function GET(request: NextRequest, {params}: {params: {id: string}}) {
     // fetch data from a db
     // if data is not found, we return a 404 error
     // else we return the data
-
-    console.log('bool:', params.id !== "pyrenees")
     if (params.id !== "pyrenees") {
         return NextResponse.json({error: "Project not found"}, {status: 404})
     }
@@ -20,8 +17,5 @@ export function GET(request: NextRequest, {params}: {params: {id: string}}) {
         subtitle: null,
         description: "Pyrénées is a photography site for Pyrénées",
         published: true   })
-
-
-    
 
 }
