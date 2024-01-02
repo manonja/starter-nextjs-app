d66cbbdc28e9645ed8f58bfa02e6c6c191511955
manonja <manon.jacquin@gmail.com>
2024-01-02 16:48:41 +0100

    feat: remove unused api route

--- app/api/projects/published/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getProjects } from "../../../../lib/prisma";
-
-// POST /api/projects/projects
-export async function GET(request: NextRequest) {
-  const result = getProjects;
-
-  return NextResponse.json({ result });
-}
